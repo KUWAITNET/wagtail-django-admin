49f1904f630a4ff71cf01355f94fe2d2b62ba92b
Ahmed ElBahnihi <abahnihi@gmail.com>
2021-06-22 09:49:34 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 def get_version(*file_paths):
-    """Retrieves the version from fsm_wagtail/__init__.py"""
+    """Retrieves the version from wagtail_django_admin/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
@@ -22,7 +22,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("fsm_wagtail", "__init__.py")
+version = get_version("wagtail_django_admin", "__init__.py")
 
 if sys.argv[-1] == 'publish':
     try:
@@ -47,7 +47,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 setup(
     name='wagtail-django-admin',
     version=version,
-    description=""" A Django app to integrate django-grappelli to wagtail admin.""",
+    description=""" A Django app to integrate django admin to wagtail admin.""",
     long_description=readme + '\n\n' + history,
     author='Ahmed ElBahnihi',
     author_email='abahnihi@gmail.com',
@@ -57,7 +57,6 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "django-grappelli==2.15.1"
     ],
     license="MIT",
     zip_safe=False,
