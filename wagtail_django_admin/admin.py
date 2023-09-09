245e9182076f5be70e4d43fe6abbcbb99062d74b
Ahmed ElBahnihi <ahmed.el-bahnihi@kuwaitnet.com>
2023-09-09 20:43:15 +0300

    fix

--- wagtail_django_admin/admin.py
+++ wagtail_django_admin/admin.py
@@ -48,8 +48,8 @@ def lang_switcher_url():
 def global_admin_js():
     django_engine = engines["django"]
     template = django_engine.get_template("wagtail_django_admin/lang_switcher.js")
+    curr_lang = get_language()
     if len(WAGTAIL_ADMIN_LANGUAGES) > 1:
-        curr_lang = get_language()
         lang = list(filter(lambda lang: lang[0] != curr_lang, WAGTAIL_ADMIN_LANGUAGES))[
             0
         ][1]
